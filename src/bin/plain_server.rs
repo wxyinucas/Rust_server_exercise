ac2d21ae365801f00a610c0428980812a253b0b0
Rex Wang <wxyinucas@gmail.com>
2022-09-20 20:50:16 +0800

    good example

--- src/bin/plain_server.rs
+++ src/bin/plain_server.rs
@@ -30,6 +30,7 @@ async fn main() {
         Ok::<_, Infallible>(service_fn(hello_world))
     });
 
+
     let server = Server::bind(&addr).serve(make_svc);
 
     // Run this server for... forever!
@@ -38,34 +39,3 @@ async fn main() {
     }
 }
 
-struct HelloWorld;
-
-impl Service<Request<Vec<u8>>> for HelloWorld {
-    type Response = Response<Vec<u8>>;
-    type Error = http::Error;
-    type Future = Pin<Box<dyn Future<Output=Result<Self::Response, Self::Error>>>>;
-
-    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        Poll::Ready(Ok(()))
-    }
-
-    fn call(&mut self, req: Request<Vec<u8>>) -> Self::Future {
-        // create the body
-        let body: Vec<u8> = "hello, world!\n"
-            .as_bytes()
-            .to_owned();
-        // Create the HTTP response
-        let resp = Response::builder()
-            .status(http::StatusCode::OK)
-            .body(body)
-            .expect("Unable to create `http::Response`");
-
-        // create a response in a future.
-        let fut = async {
-            Ok(resp)
-        };
-
-        // Return the response as an immediate future
-        Box::pin(fut)
-    }
-}
