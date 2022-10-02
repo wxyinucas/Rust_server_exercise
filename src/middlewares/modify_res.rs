b7bdf7fa58c8a5aac880fd968cc79897cbfc135e
Rex Wang <wxyinucas@gmail.com>
2022-10-02 12:19:30 +0800

    before refactor

--- src/middlewares/modify_res.rs
+++ src/middlewares/modify_res.rs
@@ -50,7 +50,7 @@ where
 {
     type Response = Response<Full<Bytes>>;
     type Error = S::Error;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;// todo why send here?
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.service.poll_ready(cx)
